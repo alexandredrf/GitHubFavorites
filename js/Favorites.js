9885f0c6563086801caa182d763cfe28e1dc2aa7
Alexandre_drf <alexandre_drf@hotmail.com>
2022-08-11 17:46:13 -0300

    criação da função createRow

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,5 +1,6 @@
-//classe que vai ter toda a nossa lógica
+//-------------> this é como estou chamando funções dentro escopo
 
+//classe que vai ter toda a nossa lógica
 export class Favorites {
     constructor(root){ //o #app vai vir parar aqui 
         //root é a div app
@@ -22,16 +23,61 @@ export class FavoritesView extends Favorites {
     //toda vez que for mudado um dado,uma função vai ser chamada
 
     update() {
-        //vamos procurar dentro da #app (root) a tbody
-        const tbody = this.root.querySelector('table tbody')
-        //entrar dentro da table tbody e pegar os seletores TR -  // como se pegasse então #app table tbody tr.
-        
-        tbody.querySelectorall('tr')
-            .forEach((tr) => { //função forEach - "para cara tr" será executado o comando abaixo 
-                //console.log(tr) //plota no console
-            tr.remove() //remove é uma função nativa do JS ela remove o tr
-            })
-       
 
+        this.removeAllTr()
+        this.createRow()
+    
     }
+
+
+    createRow(){
+    //Recriar as colunas
+    //tr vai receber os dados que vamos pegar do html
+    // para recriar uma TR, ela precisa ser criada com a DOM! https://developer.mozilla.org/pt-BR/docs/Web/API/Document_Object_Model/Introduction
+    // criando um elemento diretamente pela DOM utilizando JS
+    const tr = document.createElement('tr')
+    
+    //o que é innerHTML?!
+    // substitui o que tem dentro de um elemento por algo , nesse caso tr era vazio e assumiu o valor do texto da variável content
+
+    tr.innerHTML =`
+                    <!-- Referente a coluna usuário do thead  -->
+                    <td class="user">
+                        <img src="https://github.com/alexandredrf.png" alt="Imagem do Alexandre">
+                        <a href="https://github.com/alexandredrf" target="_blank">
+                            <p>Alexandre Davi</p>
+                            <span>alexandredrf</span>
+                        </a>
+                    </td>
+                    <!-- Referente a coluna repositório do thead  -->
+                    <td class="repositories">
+                        50
+                    </td>
+                    <!-- referente aos seguidores  -->
+                    <td class="followers">
+                        16000
+                    </td>
+                    <!-- referente ao botão de fechar  -->
+                    <td>
+                        <button class="remove">&times;</button>
+                    </td>
+                    `
+    return tr
+    //pq retornar tr ? pq vai ser utilizado pra abastercer a div tr no html
+    }
+
+    
+    removeAllTr(){
+            //vamos procurar dentro da #app (root) a tbody
+            const tbody = this.root.querySelector('table tbody')
+            //entrar dentro da table tbody e pegar os seletores TR -  // como se pegasse então #app table tbody tr.
+            
+            tbody.querySelectorall('tr')
+                .forEach((tr) => { //função forEach - "para cara tr" será executado o comando abaixo 
+                    //console.log(tr) //plota no console
+                tr.remove() //remove é uma função nativa do JS ela remove o tr
+                })
+            }
+
+
 }
